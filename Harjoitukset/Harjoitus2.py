73d353b8a4ac413248343a5834db6e5e1ff6a23e
Tomi <tomikr@metropolia.fi>
2023-01-12 15:30:07 +0200

    Tuntiharjoitteita

---
+++ Harjoitukset/Harjoitus2.py
@@ -0,0 +1,18 @@
+# presentation of variables and setting value into variable
+# name = "Tomi"
+# print(name)
+
+# reading input from the user
+# calculations and functions and changing types
+name = input("Anna nimesi: ")
+ageString = input("Anna ikäsi: ")
+# Tyyppimuunnos merkkijonosta (string) kokonaisuvuksi (int)
+age = int(ageString)
+age = age + 1
+message = "Terve, " + name + "! Olet ensi vuonna " + str(age) + "-vuotias"
+# sama f-stringiä käyttäen
+message = f"Terve, {name}! Olet ensi vuonna {age}-vuotias"
+print(message)
+
+
+
