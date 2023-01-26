4ad3df691b2c584fb090c55ee9f8dfaa43f1f839
HerraHyy <tomikr@metropolia.fi>
2023-01-26 23:48:19 +0200

    260123 update

--- Moduuli-05/Teht1.py
+++ Moduuli-05/Teht1.py
@@ -3,6 +3,8 @@ Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän. Ohjel
 tulostaa silmälukujen summan. Käytä for-toistorakennetta.
 """
 import random
-n = int(input("Anna arpakuutioiden lukumäärä 1-10: "))
-h = random.randint(1, 6)
-for n in
+n = int(input("Anna arpakuutioiden lukumäärä: "))
+sum = 0
+for i in range(n):
+    sum += random.randint(1, 6)
+print(f"Arpakuutioiden silmälukujen summa on {sum}")
